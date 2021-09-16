ee258ff1aa198f90ae13e637c5294efe43c2d0be
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-09-16 14:58:01 -0600

    Update button colors and hover depending on dark or light mode

--- index.js
+++ index.js
@@ -4,34 +4,44 @@ const apps = [
   'shop',
   'note',
   'game',
-  'graveyard',
+  'grave',
 ];
 
 let appName = document.getElementById('app-name');
 appName.innerText = apps[0];
+const clickable = document.querySelectorAll('.clickable')
 
 const toggleDark = () => {
   document.body.style.backgroundColor = '#000000';
-  document.body.style.color = '#FFFFFF';
+  document.body.style.color = 'grey';
   appName.style.color = 'purple';
+  clickable.forEach(el => {
+    el.classList.remove('clickable');
+    el.classList.add('clickable-dark');
+  });
 };
 
 const toggleLight = () => {
   document.body.style.backgroundColor = '#FFFFFF';
   document.body.style.color = '#000000';
   appName.style.color = 'teal';
+  clickable.forEach(el => {
+    el.classList.remove('clickable-dark');
+    el.classList.add('clickable');
+  });
 };
 
 const rotateRight = () => {
   apps.push(apps.shift());
   appName.innerText = apps[0];
-  apps[0] === 'graveyard' ? toggleDark() : toggleLight();
+  apps[0] === 'grave' ? toggleDark() : toggleLight();
   return apps;
 };
 
 const rotateLeft = () => {
   apps.unshift(apps.pop());
   appName.innerText = apps[0];
+  apps[0] === 'grave' ? toggleDark() : toggleLight();
   return apps;
 };
 
