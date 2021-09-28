d19cea664853263b3e560aa486b18de2c85518bc
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-09-28 17:41:16 -0600

    Add pocket game

---
+++ pocket_game/game.js
@@ -0,0 +1,116 @@
+// initial state for the game
+// they are equal to each other, any change to one happens to the other
+px = py = 10; // head - used to reference every segmetn
+gs = ts = 20; // game boundaries -> size of playable area - number of pixels in each square
+ax = ay = 15; // apple
+
+// only values manipulated by our keys (directional values)
+xv = yv = 0; // x value and y value coordinates
+
+body = []; // entire body, each segment: { x: px, y: py }
+segments = 5; // segments of snake's body
+
+
+// logic for game
+const game = () => {
+  let gameScore = document.getElementById('game-score')
+  gameScore.innerHTML = `${segments - 5}`
+  let highScore = document.getElementById('high-score')
+
+  // console.log(highScore.innerHTML, typeof highScore.innerHTML)
+  if (+gameScore.innerHTML > +highScore.innerHTML) {
+    highScore.innerHTML = `${gameScore.innerHTML}`
+  }
+  px += xv;
+  py += yv;
+
+  context.fillStyle = '#0c1a29';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = '#49c86f';
+
+  // reset if snek comes off edge of board
+  if (px < 0) px = ts - 1;
+  if (px > ts - 1) px = 0;
+  if (py < 0) py = ts - 1;
+  if (py > ts - 1) py = 0;
+
+  for (let i = 0; i < body.length; i++) {
+    context.fillRect(body[i].x * gs, body[i].y * gs, gs - 2, gs - 2);
+    // if head intersepts any segments of the body, reset segments to 5
+    if (body[i].x === px && body[i].y === py) {
+      segments = 5;
+    }
+  };
+
+  // on initial render, push in the head of the snek
+  body.push({ x: px, y: py });
+
+  // if we reset back to 5 segments, knock off the rest of the segments
+  while (body.length > segments) {
+    body.shift();
+  };
+
+  // eating
+  // if head finds apple, are occupying the same space
+  if (ax === px && ay === py) {
+    // add onto the body
+    segments++;
+    // set new apple somewhere else
+    ax = Math.floor(Math.random() * ts);
+    ay = Math.floor(Math.random() * ts);
+  };
+
+  context.fillStyle = '#ff5353';
+  context.fillRect(ax * gs, ay * gs, gs - 2, gs - 2);
+};
+
+
+
+// control our D-Pad
+const keyDown = e => {
+  const instructions = document.getElementById('instructions');
+  switch (e.keyCode) {
+    case 65:
+    case 37:
+      // hides message
+      instructions.setAttribute('hidden', 'true')
+      // can't go in the opposite direction
+      if (xv === 1) break;
+      xv = -1;
+      yv = 0;
+      break;
+    case 87:
+    case 38:
+      instructions.setAttribute('hidden', 'true')
+      if (yv === 1) break;
+      xv = 0;
+      yv = -1;
+      break;
+    case 68:
+    case 39:
+      instructions.setAttribute('hidden', 'true')
+      if (xv === -1) break;
+      xv = 1;
+      yv = 0;
+      break;
+    case 83:
+    case 40:
+      instructions.setAttribute('hidden', 'true')
+      if (yv === -1) break;
+      xv = 0;
+      yv = 1;
+      break;
+  }
+};
+
+
+const init = () => {
+  canvas = document.getElementById('game');
+  context = canvas.getContext('2d');
+  document.addEventListener('keydown', keyDown);
+  setInterval(game, 100);
+}
+
+// window.onload = init()
+// or
+document.addEventListener('DOMContentLoaded', init);
